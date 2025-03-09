2aa78ab7358f8f063f81a351f308f5cc74ee5b60
iamsusmitashrestha <iamsusmitashrestha@gmail.com>
2025-03-09 15:27:57 +0545

    Write migration for create user

---
+++ server/src/db/migrations/20250309045858_create_users.sql
@@ -0,0 +1,18 @@
+-- Up: Create Table
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(500) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    dob DATETIME NOT NULL,
+    gender ENUM('M','F','O') NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    role ENUM('super_admin','artist_manager','artist') NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Down: Drop Table
+DROP TABLE IF EXISTS users;
